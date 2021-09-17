91d18ec9609ddaadb057dd7146e35986824f15b7
TakayasuKoura <5574536+TakayasuKoura@users.noreply.github.com>
2021-09-17 06:46:29 +0000

    Remove unused files

--- frontend/src/pages/api/datasets/categories.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import categories from '../../../../public/data/categories.json';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(categories);
-}
--- frontend/src/pages/api/datasets/types.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import types from '../../../../public/data/types.json';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(types);
-}
