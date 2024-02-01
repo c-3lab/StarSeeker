a9c881e58fe27ca2cceeeafaafb0552448734e5c
i-kaneko-abel <i-kaneko@abelsoft.co.jp>
2024-02-01 10:40:50 +0900

    Setting initial values ​​of map parameters

--- StarSeeker/frontend/src/components/Map.tsx
+++ StarSeeker/frontend/src/components/Map.tsx
@@ -5,11 +5,30 @@ import 'leaflet/dist/leaflet.css';
 import DisplayPoints from './DisplayPoints';
 import DisplaySurfaces from './DisplaySurfaces';
 
-const defaultPosition: LatLngTuple = [
-  process.env.NEXT_PUBLIC_MAP_DEFAULT_LATITUDE,
-  process.env.NEXT_PUBLIC_MAP_DEFAULT_LONGITUDE,
-];
-const defalutZoom: number = process.env.NEXT_PUBLIC_MAP_DEFAULT_ZOOM;
+const loadEnvAsNumber = (
+  variable: string | undefined,
+  defaultValue: number
+): number => {
+  if (!variable) return defaultValue;
+  const value = parseFloat(variable);
+  if (Number.isNaN(value)) return defaultValue;
+  return value;
+};
+
+const defaultLatitude = loadEnvAsNumber(
+  String(process.env.NEXT_PUBLIC_MAP_DEFAULT_LATITUDE),
+  35.6581107
+);
+const defaultLongitude = loadEnvAsNumber(
+  String(process.env.NEXT_PUBLIC_MAP_DEFAULT_LONGITUDE),
+  139.7387888
+);
+const defaultZoom = loadEnvAsNumber(
+  String(process.env.NEXT_PUBLIC_MAP_DEFAULT_ZOOM),
+  13
+);
+
+const defaultPosition: LatLngTuple = [defaultLatitude, defaultLongitude];
 
 type Props = {
   pointEntities: any[];
@@ -27,7 +46,7 @@ const Map: React.VFC<Props> = ({ pointEntities, surfaceEntities, fiware }) => {
   return (
     <MapContainer
       center={defaultPosition}
-      zoom={defalutZoom}
+      zoom={defaultZoom}
       scrollWheelZoom={true}
       zoomControl={false}
     >
